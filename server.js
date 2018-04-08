b61f17da1ae426968bdeb9bcf724cdbb84a1ecb1
Ryan Hafen <ryan.hafen@icloud.com>
2018-04-08 03:47:49 -0700

    final

---
+++ server.js
@@ -0,0 +1,57 @@
+// ================================================================
+// tools
+// ================================================================
+const dotenv = require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const port = 3000;
+
+const app = express()
+
+// ================================================================
+// setup express application
+// ================================================================
+const ApiKey = process.env.API_KEY;
+	// console.log(ApiKey);
+const baseUrl = "http://api.wunderground.com/api/" + ApiKey + "/forecast10day/geolookup/conditions/hourly/q/"
+	// console.log(baseUrl);
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+// ================================================================
+// setup routes
+// ================================================================
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+	var state = req.body.state;
+	var city = req.body.city;
+	var url = baseUrl + state + '/' + city + '.json';
+	var radarUrl = "http://api.wunderground.com/api/"+ ApiKey +"/animatedradar/animatedsatellite/q/"+ state +"/"+ city +".gif?num=6&delay=50&interval=30"
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.location == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+		let weatherData = weather;
+        res.render('index', {weather: weatherData, radar: radarUrl, error: null});
+      }
+    }
+  });
+})
+
+// ================================================================
+// start our server
+// ================================================================
+app.listen(port, function () {
+  console.log('App listening on port 3000!')
+})
