f5ddbf5cfaac406611be2b2e9bfb8da618105a43
Ryan Hafen <ryan.hafen@icloud.com>
2018-04-08 10:54:12 -0700

    fix app

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.post('/', function (req, res) {
 // ================================================================
 // start our server
 // ================================================================
-app.listen(port, function () {
-  console.log('App listening on port 3000!')
-})
+app.listen(process.env.PORT || port, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
